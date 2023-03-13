5eceb487a4fe3c3369ddbb6e9bd42102696b3e89
abdullahyaqoob <65088172+abdullahyaqoob@users.noreply.github.com>
2023-03-13 17:27:29 +1100

    Add files via upload

---
+++ build/precache-manifest.66ea0b2381a9c8757db92f149ceca0c3.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab193725d6f07e14d790",
+    "url": "/static/css/main.b36f9783.chunk.css"
+  },
+  {
+    "revision": "ab193725d6f07e14d790",
+    "url": "/static/js/main.ab193725.chunk.js"
+  },
+  {
+    "revision": "0a642b4487087e254fb5",
+    "url": "/static/css/1.75b49b1b.chunk.css"
+  },
+  {
+    "revision": "0a642b4487087e254fb5",
+    "url": "/static/js/1.0a642b44.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7eebdb8b7813a0e88a83962a043aaa16",
+    "url": "/static/media/laExchange.7eebdb8b.png"
+  },
+  {
+    "revision": "78c9cc6fb908bb5a09b40e0569fdcf8c",
+    "url": "/static/media/footerLogo.78c9cc6f.png"
+  },
+  {
+    "revision": "b1a0ca6a2f0b2ad1e8701564fe6b21fa",
+    "url": "/static/media/graphDesktop.b1a0ca6a.png"
+  },
+  {
+    "revision": "6b1844e73497fd609ec8cb34e1a28f3b",
+    "url": "/static/media/graphArtificalView.6b1844e7.png"
+  },
+  {
+    "revision": "a233a523da9b145fba9cd29e1c59a776",
+    "url": "/static/media/binanceSmartChain.a233a523.png"
+  },
+  {
+    "revision": "7ec48c982a3108cd8dc469063197a408",
+    "url": "/static/media/bottomPageLogo.7ec48c98.png"
+  },
+  {
+    "revision": "d3bdeb52b391a65663cbae626deb7fb5",
+    "url": "/static/media/metamaskIcon.d3bdeb52.png"
+  },
+  {
+    "revision": "d343595b34c1bd78b996b02f3301b98b",
+    "url": "/static/media/polygonIcon.d343595b.png"
+  },
+  {
+    "revision": "29c82cfe4b4e45b15863d8853bab21dd",
+    "url": "/static/media/box1.29c82cfe.png"
+  },
+  {
+    "revision": "40cdfeee1a98ad5d52ac0c9286a4befd",
+    "url": "/static/media/box2.40cdfeee.png"
+  },
+  {
+    "revision": "7da061e2eaa3c5a013424dfc5585b3b2",
+    "url": "/static/media/box3.7da061e2.png"
+  },
+  {
+    "revision": "d3d611bc27698cfb0899b5f6146d581e",
+    "url": "/static/media/exchange1stBox.d3d611bc.png"
+  },
+  {
+    "revision": "6783694eed9407cc37f5ed11ea887b5b",
+    "url": "/static/media/exchange2ndBox.6783694e.png"
+  },
+  {
+    "revision": "922086359d956500d43dec8e0901a31f",
+    "url": "/static/media/exchange3rdBox.92208635.png"
+  },
+  {
+    "revision": "7f646abde404300bdf468969dcb18a88",
+    "url": "/index.html"
+  }
+];
